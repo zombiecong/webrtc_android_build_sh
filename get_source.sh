c3e2fca44afa048aa574f0efa7a899a167083c74
Chen Cong <ccaelums@gmail.com>
2016-03-09 14:49:57 +0800

    add rm androidapp

--- get_source.sh
+++ get_source.sh
@@ -18,7 +18,7 @@ WEBRTC_JAVA_API_PATH="$WEBRTC_ROOT/src/webrtc/api/java/src/org/webrtc"
 WEBRTC_ANDROID_SDK_PATH="$WEBRTC_ROOT/sdk_src"
 
 function clean() {
-	rm -rf "$WEBRTC_ANDROID_SDK_PATH/"*
+	rm -rf "$WEBRTC_ANDROID_SDK_PATH/"* androidapp/*
 }
 
 clean
@@ -36,13 +36,13 @@ else
 fi
 
 if [ ! -d "VOICE_ENGINE_SOURCE_PATH" ]; then
-	cp -rf "$VOICE_ENGINE_SOURCE_PATH" "$WEBRTC_ANDROID_SDK_PATH/"
+	cp -rf "$VOICE_ENGINE_SOURCE_PATH" "$WEBRTC_ANDROID_SDK_PATH/voiceengine"
 else
 	echo  voiceengine path not exist
 fi
 
 if [ ! -d "VIDEO_ENGINE_SOURCE_PATH" ]; then
-	cp -rf "$VIDEO_ENGINE_SOURCE_PATH" "$WEBRTC_ANDROID_SDK_PATH/"
+	cp -rf "$VIDEO_ENGINE_SOURCE_PATH" "$WEBRTC_ANDROID_SDK_PATH/videoengine"
 else
 	echo  videoengine path not exist
 fi
